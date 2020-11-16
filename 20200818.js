5841533540e643bf29b136a5f24a18cf6481a6eb
amalyshkin <alex@bigbrassband.com>
2020-11-16 17:07:01 +0600

    TST-4 link by comm 1

--- 20200818.js
+++ 20200818.js
@@ -3,6 +3,7 @@ package modules;
 import org.openqa.selenium.Ru;
 import org.openqa.selenium.Ua;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ZZ;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
